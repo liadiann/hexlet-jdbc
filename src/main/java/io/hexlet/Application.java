92167bbc0cd7c7683ee38e09d9eae8656a930794
Julia Kon <kon1976y@gmail.com>
2025-07-11 11:11:19 +0300

    update Application

--- src/main/java/io/hexlet/Application.java
+++ src/main/java/io/hexlet/Application.java
@@ -2,6 +2,7 @@ package io.hexlet;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
 
@@ -16,9 +17,24 @@ public class Application {
             try (var statement = conn.createStatement()) {
                 statement.execute(sql);
             }   // В конце закрываем
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+            /*var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
             try (var statement2 = conn.createStatement()) {
                 statement2.executeUpdate(sql2);
+            }*/
+            var sql2 = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sql2, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Ellie");
+                preparedStatement.setString(2, "123456789");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Joel");
+                preparedStatement.setString(2, "987654321");
+                preparedStatement.executeUpdate();
+
+                /*var generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    System.out.println(generatedKeys.getLong(1));
+                }*/
             }
             var sql3 = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
@@ -26,6 +42,18 @@ public class Application {
                 var resultSet = statement3.executeQuery(sql3);
             // Набор данных — это итератор
             // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
+                while (resultSet.next()) {
+                    System.out.println(resultSet.getString("username"));
+                    System.out.println(resultSet.getString("phone"));
+                }
+            }
+            var sql4 = "DELETE FROM users WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(sql4, Statement.RETURN_GENERATED_KEYS)) {
+                preparedStatement.setString(1, "Joel");
+                preparedStatement.executeUpdate();
+            }
+            try (var statement4 = conn.createStatement()) {
+                var resultSet = statement4.executeQuery(sql3);
                 while (resultSet.next()) {
                     System.out.println(resultSet.getString("username"));
                     System.out.println(resultSet.getString("phone"));
